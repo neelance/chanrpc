0acfe8ebbaccfc71fb81f67077363999338975b9
Richard Musiol <mail@richard-musiol.de>
2016-04-04 17:14:40 +0200

    turn chanrpcutil.ToChunks into two simpler functions

--- chanrpcutil/bytes.go
+++ chanrpcutil/bytes.go
@@ -20,19 +20,27 @@ func ReadAll(c <-chan []byte) <-chan []byte {
 	return c2
 }
 
-func ToChunks(b []byte) <-chan []byte {
-	return ToChunksSize(b, 1024*1024)
+func SplitToChunks(b []byte) [][]byte {
+	return SplitToChunksSize(b, 1024*1024)
 }
 
-func ToChunksSize(b []byte, chunkSize int) <-chan []byte {
-	c := make(chan []byte, 10)
-	go func() {
-		for len(b) > chunkSize {
-			c <- b[:chunkSize]
-			b = b[chunkSize:]
+func SplitToChunksSize(b []byte, chunkSize int) [][]byte {
+	chunks := make([][]byte, (len(b)+chunkSize-1)/chunkSize)
+	for i := range chunks {
+		if i == len(chunks)-1 {
+			chunks[i] = b[i*chunkSize:]
+			break
 		}
+		chunks[i] = b[i*chunkSize : (i+1)*chunkSize]
+	}
+	return chunks
+}
+
+func SendChunksOnChannel(chunks [][]byte) <-chan []byte {
+	c := make(chan []byte, len(chunks))
+	for _, b := range chunks {
 		c <- b
-		close(c)
-	}()
+	}
+	close(c)
 	return c
 }
